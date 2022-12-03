c3a8d704e4a7497f5a4e946a59951cdc3bcd75e4
Monnapse <76830589+Monnapse@users.noreply.github.com>
2022-12-03 10:30:47 -0700

    Update Astro.lua

--- Astro.lua
+++ Astro.lua
@@ -1,1 +1,33 @@
+--[[
 
+	Astro
+	Made by Monnapse#7578
+	
+	Astro is a Modern UI library
+
+--]]
+
+--// Directories
+local Elements = script.Elements
+
+local astro = {}
+
+function astro.new()
+	local self = {}
+
+	self.Elements             = {}
+	self.Elements.TextButton  = require(Elements.Button).text
+	self.Elements.ImageButton = require(Elements.Button).text
+	
+	--// Directories
+	self.GUI = Instance.new("ScreenGui")
+	
+	--// Elements Creating
+	function self:CreateElement(Element)
+		Element()
+	end
+	
+	return self
+end
+
+return astro
